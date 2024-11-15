836170beacd63c28398cfa3df77dba6972e04d5d
RAAVAN <85153666+RAAVANUNKNOWN10@users.noreply.github.com>
2024-11-15 17:54:08 +0530

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -3,15 +3,15 @@ echo "updating syatem"
 sudo apt update && sudo apt upgrade -y
 echo "installing python3 and pip3"
 sudo apt install python3 python3-pip3 -y
-apt install python3 python3-pip3 -y
+#apt install python3 python3-pip3 -y
 
 #installing mysql server
 # Install MySQL Server
-echo "Installing MySQL Server..."
+#echo "Installing MySQL Server..."
 #sudo apt install -y mysql-server
 
 # Secure MySQL installation
-echo "Securing MySQL installation..."
+#echo "Securing MySQL installation..."
 #sudo mysql_secure_installation
 
 # Install SQLite
@@ -20,8 +20,8 @@ sudo apt install -y sqlite3
 #apt install -y sqlite3
 
 # Check the installed versions
-echo "MySQL version:"
-mysql --version
+#echo "MySQL version:"
+#mysql --version
 
 echo "SQLite version:"
 sqlite3 --version
@@ -29,7 +29,7 @@ sqlite3 --version
 #sudo systemctl status mysql
 echo "mysql services has been started"
 #open my sql server
-echo "Opening my sql server"
+#echo "Opening my sql server"
 #sudo mysql -u root -p
 #main packages
 echo "installing pyqt6, mysqlconector,pyside for runing application"
@@ -43,11 +43,14 @@ echo "~/myapp dic was created"
 sleep 5s
 cd ~/myapp/
 git clone https://github.com/RAAVANUNKNOWN10/linux-py.git
+echo "cloned repo"
+##############################################################
 mv ./linux-py/myapp.py ~/myapp/linux-py/login.py
+mv ./linux-py/login.desktop ~/Desktop
+echo "moved file on desktop and rename the .py file"
 #sudo mv myapp.py ~/myapp/login.py
 
 echo "giving permission of exicution"
-mv ./linux-py/login.desktop ~/Desktop
 # creating desk top file
 
 sleep 2s
@@ -59,12 +62,8 @@ sleep 2s
 #sudo python3 ~/myapp/login.py
 
 #EOF'
-cd ~/myapp/linux-py/
-chmod 777 login.py
-chmod 111 login_desktop.sh
-
-cd ~/Desktop
-
-chmod 111 ./myapp.desktop
+sudo chmod 777 ~/myapp/linux-py/login.py
+sudo chmod 111 ~/myapp/linux-py/login_desktop.sh
+sudo chmod 111 ~/Desktop/login.desktop
 
 echo "application stared"
